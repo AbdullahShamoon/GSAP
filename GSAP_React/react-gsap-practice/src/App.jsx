5391a81e1197e54c1939ae4b26793cbb37024101
Abdullah <abdullahshamoon24@gmail.com>
2024-12-27 20:41:35 +0530

    basic gsap

--- GSAP_React/react-gsap-practice/src/App.jsx
+++ GSAP_React/react-gsap-practice/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
 
-function App() {
-  const [count, setCount] = useState(0)
+gsap.registerPlugin(useGSAP);
+
+
+const App = () => {
+
+  useGSAP(
+    () => {
+      // gsap code here...
+      gsap.to('.box', {
+        x: 1000,
+        duration: 3,
+        rotateX: 360,
+        rotateY: 360,
+        rotateZ: 360,
+        
+      }); // <-- automatically reverted
+      gsap.to('.circle', {
+        x:1000,
+        y:50,
+        duration:5,
+        rotateY: 700,
+        scale: 2
+      })
+    });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div className="box">BOX</div>
+      <div className="circle">CIRCLE</div>
+
+    </div>
   )
 }
 
